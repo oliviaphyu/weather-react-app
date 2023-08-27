01f3e5a6f15d37aef43c37ccecf3c1d6e695e87c
Phyu Sin Lin <122966730+oliviaphyu@users.noreply.github.com>
2023-08-27 21:42:26 +0630

    Added the GitHub repo URL link

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Phyu Sin Lin and is{" "}
+        <a href="https://github.com/oliviaphyu/weather-react-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
